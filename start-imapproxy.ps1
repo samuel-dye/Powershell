5a5e6babc5860ba09a5f7ec2b9e794c345b0f6db
Samuel Dye <37191055+samuel-dye@users.noreply.github.com>
2018-10-09 09:18:16 -0500

    Create start-imapproxy.ps1

---
+++ start-imapproxy.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+    Checks the health & status of the IMAP Proxy service, and starts it if not healthy and online.
+
+.NOTES
+    CREATE DATE:    2018-10-09
+    CREATE AUTHOR:  Sam Dye
+    REV NOTES:
+            v1.0 - Completed script and deployed via Task Schedulder to run on startup, after a 15 minute delay.
+       
+#>
+
+# add exchange management module
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
+# check status, and start if NOT already online
+Get-HealthReport -Identity "$servername" | where {$_.HealthSet -eq "IMAP.Proxy"}
+    if ({$_.State -ne "Online"}) {
+            Set-ServerComponentState -Identity "ARJ-EXCH01" -Component "imapproxy" -Requester "HealthAPI" -State "Active"
+        }
+        else {
+            exit
+        }
